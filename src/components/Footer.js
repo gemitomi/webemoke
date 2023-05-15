733b499cc88b5361db66c3187ac2305a5b574eda
Tom Gemi <gemitomi@hotmail.com>
2023-05-15 13:18:48 +0200

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,33 @@
 import React from "react";
+import SocialLinks from "./SocialLinks";
 
 export default function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <section class="footer">
+        <div class="box-container">
+          <div class="box">
+              <a href="#"> <i class="fas fa-angle-right"></i> GYIK</a>
+              <a href="#"> <i class="fas fa-angle-right"></i> Adatkezelési Tájékoztató</a>
+              <a href="#"> <i class="fas fa-angle-right"></i> Impressum</a>
+          </div>
+
+          <div class="box">
+              <a href="#"> <i class="fas fa-phone"></i> +36-70-948-5481</a>
+              <a href="#"> <i class="fas fa-envelope"></i> bekes.emoke@gmail.com</a>
+              <a href="#"> <i class="fas fa-map"></i> Budaörs, Napsugár sétány 4. II/13.</a>
+          </div>
+
+          <div class="box">
+              <a href="https://hu-hu.facebook.com/people/B%C3%A9k%C3%A9s-Em%C5%91ke-csal%C3%A1dk%C3%ADs%C3%A9r%C5%91-d%C3%BAla/100042636705062/"> <i class="facebook"><SocialLinks/></i></a>
+          </div>
+        </div>
+
+        <div class="credit"> Copyright ©2023 <a href="https://webrabbit.hu">WebRabbit</a> | All rights reserved</div>
+      </section>
+      
+        
+    </footer>
   )
 }
 
---
+++ src/components/SocialLinks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SocialLinks() {
+    return (
+  
+        <div className='social-links-container'>
+            <i class="fa fa-facebook-official" aria-hidden="true"></i>
+        </div>
+
+    )
+}
