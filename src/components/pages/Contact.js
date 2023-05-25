d6062b330cb662252f0d784e7381d9bfca2d7270
Tom Gemi <gemitomi@hotmail.com>
2023-05-25 23:46:02 +0200

    mail sending & contact resp

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,6 @@
 import React, {useRef} from 'react'
+import emailjs from '@emailjs/browser';
+import { Button } from '@mui/material';
 
 
 
@@ -7,11 +9,13 @@ import Mail1 from "../../image/mail_1.png"
 import Call1 from "../../image/call_1.png"
 
 export default function Contact() {
-  const form = useRef()
+  
 
-  const sendEmail = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
     
-  };
+    emailjs.sendForm('service_nclcu9j', 'template_f8fh6j8', e.target, 'dvGDZxVnl8xRc5665');
+  }
    
   return (
     <div>
@@ -39,35 +43,36 @@ export default function Contact() {
         </div>
             <div class="contactForm">
               <h2 >Kapcsolatfelvétel</h2>
-              <form ref={form} onSubmit={sendEmail}>
+              <form onSubmit={sendEmail}>
                 <div class="formBox">
+                  
                   <div class="inputBox w50">
-                    <input type="text" id="surname" required/>
+                    <input type="text" name="surname"  id="surname" required/>
                     <span>Vezetéknév</span>
                   </div>
 
                   <div class="inputBox w50">
-                      <input type="text" id="firstname" required/>
+                      <input type="text" name="firstname" id="firstname" required/>
                       <span>Keresztnév</span>
                   </div>
-
+                  
                   <div class="inputBox w50">
-                      <input type="text" id="email" required/>
+                      <input type="text" name="email_from" id="emailFrom" required/>
                       <span>e-mail cím</span>
                   </div>
 
                   <div class="inputBox w50">
-                      <input type="text" id="phone" required/>
+                      <input type="text" name="phone" id="phone" required/>
                       <span>Mobilszám</span>
                   </div>
 
                   <div class="inputBox w100">
-                      <textarea id="message" required></textarea>
-                      <span>Kérem küldjön nekem üzenetet itt...</span>
+                      <textarea name="message" id="message" required></textarea>
+                      <label htmlFor='message'>Kérem küldjön nekem üzenetet itt...</label>
                   </div>
 
                   <div class="inputBox w50">
-                      <input type="submit" value="Küldés" />
+                      <Button type="submit" >Küldés</Button>
                   </div>
                 </div>
               </form>
--- src/components/pages/Dulakor.js
+++ src/components/pages/Dulakor.js
@@ -3,3 +3,28 @@ import React from "react";
 export default function Dulakor() {
   return <div>Dulakor</div>;
 }
+/*<div class="inputBox w50">
+                    <input type="text"  id="surname" required/>
+                    <span>Vezetéknév</span>
+                  </div>
+
+                  <div class="inputBox w50">
+                      <input type="text" id="firstname" required/>
+                      <span>Keresztnév</span>
+                  </div>
+                  
+                  
+                  ide a mail
+                  
+                  
+                <div class="inputBox w50">
+                      <input type="text" id="phone" required/>
+                      <span>Mobilszám</span>
+                  </div>  
+                  
+                  
+                  
+                  
+                  
+                  
+                  */
